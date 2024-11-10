983890ecd440b75249b03d2131fb85498f0b9dfd
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2024-11-11 02:12:16 +0700

    update 3 files, create 2 files, delete 2 files, copy 1 file and move 1 file

---
+++ backend/lib/active_record_ulid/railtie.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'rails'
+require 'rails/generators'
+require 'rails/generators/migration'
+
+# = Ulid Railtie
+#
+# Creates a new railtie for 2 reasons:
+#
+# * Initialize ActiveRecord properly
+# * Add active_record_ulid:install generator
+class ActiveRecordUlid < Rails::Railtie
+  initializer 'active_record_ulid' do
+    ActiveSupport.on_load :active_record_postgresqladapter do
+      require 'active_record'
+      require 'active_record/connection_adapters/postgresql_adapter'
+      require 'active_support'
+      require_relative 'oid/ulid'
+      require_relative 'schema_defination'
+      require_relative 'adapters'
+      require_relative 'quoting'
+
+      ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:ulid] = { name: 'ulid' }
+
+      ActiveSupport.on_load(:active_record_postgresqladapter) do
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(ActiveRecordULID::Adapters)
+        ActiveRecord::ConnectionAdapters::PostgreSQL::TableDefinition.include(ActiveRecordULID::ColumnMethods)
+        ActiveRecord::ConnectionAdapters::PostgreSQL::Quoting.prepend(ActiveRecordULID::Quoting)
+      end
+    end
+  end
+
+  # Creates the active_record_ulid:install generator. This generator creates a migration that
+  # adds active_record_ulid support for your database. If fact, it's just the sql from the
+  # contrib inside a migration. But it' s handy, isn't it?
+  #
+  # To use your generator, simply run it in your project:
+  #
+  #   rails g active_record_ulid:install
+  class Install < Rails::Generators::Base
+    include ::Rails::Generators::Migration
+
+    def self.source_root
+      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    end
+
+    def self.next_migration_number(dirname)
+      format('%.3d', (current_migration_number(dirname) + 1))
+    end
+
+    def create_migration_file
+      pgversion = ::ActiveRecord::Base.connection.send(:postgresql_version)
+      throw 'Your PostgreSQL version is not supported, require Postgre >= 14.0' unless pgversion > 140_000
+
+      migration_template 'setup_ulid.rb', 'db/migrate/setup_ulid.rb'
+
+      puts <<~EOS
+        You're all set!
+        Add this changes to "#{Rails.root}/config/application.rb"
+        ```
+        class Application < Rails::Application\e[32m
+          config.active_record.primary_key = :ulid
+          config.generators do |g|
+            g.orm :active_record, primary_key_type: :ulid
+            g.orm :active_record, foreign_key_type: :ulid
+          end\e[0m
+        end
+        ```
+      EOS
+    end
+  end
+end
--- backend/lib/json_hash.gemspec
+++ backend/lib/json_hash.gemspec
@@ -1,30 +1,30 @@
 # rubocop:disable Gemspec/DevelopmentDependencies
 # frozen_string_literal: true
 
+require_relative 'json_hash/json_hash'
+
 Gem::Specification.new do |s|
-  s.name = 'active_record_ulid'
+  s.name = 'json_hash'
   s.version = '0.1.0'
 
   s.platform    = Gem::Platform::RUBY
   s.license     = 'MIT'
   s.authors     = ['Ho Trung Nhan']
   s.email       = 'hotrungnhan29@gmail.com'
-  s.homepage    = 'http://github.com/hotrungnhan/active-record-ulid'
+  s.homepage    = 'http://github.com/hotrungnhan/ruby_json_hash'
   s.summary     = 'Goodbye uuid, hello ulid'
   s.description = ''
   s.required_ruby_version     = '>= 3.2.0'
   s.required_rubygems_version = '>= 3.5.0'
 
-  s.add_runtime_dependency 'activerecord', '>= 7.1'
-  s.add_runtime_dependency 'activesupport', '>= 7.1'
-  s.add_runtime_dependency 'ulid'
+  s.add_runtime_dependency 'oj'
   s.add_development_dependency 'bundler'
   s.add_development_dependency 'rdoc'
   s.add_development_dependency 'rspec', '>= 3.0'
 
-  s.files              =['active_record_ulid/**/*']
+  s.files              =['json_hash/json_hash']
   s.executables        = []
-  s.require_paths      = %w[active_record_ulid]
+  s.require_paths      = %w[json_hash]
   s.metadata['rubygems_mfa_required'] = 'true'
 end
 
